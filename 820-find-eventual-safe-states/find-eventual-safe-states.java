8ae6cd0bf215fce33be216e52ce31162960b26de
Utkarsh Singh <98802787+utkarshsingh17@users.noreply.github.com>
2025-01-24 23:12:00 +0530

    Time: 4 ms (99.02%) | Memory: 55.7 MB (24.54%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,15 +1,12 @@
 class Solution {
     public static boolean isCycle(int[][]graph,boolean[] vis, boolean[] pathvis,int node){
-        if(pathvis[node]==true)return true;
-        if(vis[node]==true)return false;
         vis[node]=true;
         pathvis[node]=true;
         for(int nbr:graph[node]){
-                //pathvis[nbr]=true;
-                //vis[nbr]=true;
+            if(vis[nbr]==false){
                 if(isCycle(graph,vis,pathvis,nbr)==true)return true;
-            
-            //else if(pathvis[nbr]==true)return true;
+            }
+            else if(pathvis[nbr]==true)return true;
         }
         pathvis[node]=false;
         return false;
@@ -19,8 +16,11 @@ class Solution {
         boolean pathvis[]=new boolean[graph.length];
         List<Integer> res=new ArrayList<>();
         for(int i=0;i<graph.length;i++){
-            isCycle(graph,vis,pathvis,i);
-             
+            if(vis[i]==false){
+                isCycle(graph,vis,pathvis,i);
+            }
+            
+            
         }
         for(int i=0;i<pathvis.length;i++){
             if(!pathvis[i])res.add(i);
