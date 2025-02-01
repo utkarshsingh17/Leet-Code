3f3c577a06d8bbb0380d67124b624529b74f4505
Utkarsh Singh <98802787+utkarshsingh17@users.noreply.github.com>
2025-02-02 02:15:16 +0530

    Time: 1 ms (37.43%) | Memory: 43.5 MB (43.77%) - LeetSync

---
+++ 3429-special-array-i/special-array-i.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n-1;i++){
+            if((nums[i]%2==0&&nums[i+1]%2==0)||(nums[i]%2==1&&nums[i+1]%2==1))return false;
+        }
+        return true;
+    }
+}
