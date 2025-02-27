f1a22f0fbee6b6d18e7dd9285468489974c4535a
Utkarsh Singh <98802787+utkarshsingh17@users.noreply.github.com>
2025-02-28 00:05:43 +0530

    Time: 191 ms (36.09%) | Memory: 57.9 MB (5.03%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
@@ -1,30 +1,25 @@
 class Solution {
     public int lenLongestFibSubseq(int[] arr) {
+        int cache[][]=new int[arr.length][arr.length];
+        for(int[] c:cache){
+            Arrays.fill(c,2);
+        }
         Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<arr.length;i++){
             map.put(arr[i],i);
         }
-        int len=0,res=0;
-        int cache[][]=new int[arr.length][arr.length];
-        for(int[] c:cache){
-            Arrays.fill(c,-1);
-        }
-        for(int k=arr.length-1;k>0;k--){
-            for(int j=k-1;j>0;j--){
-                len=solve(j,k,arr,map,cache);
-                if(len>=3){
-                    res=Math.max(res,len);
+        int res=0;
+        for(int j=1;j<arr.length;j++){
+            for(int k=j+1;k<arr.length;k++){
+                int t=arr[k]-arr[j];
+                if(map.containsKey(t) && map.get(t)<j){
+                    int i=map.get(t);
+                    cache[j][k]=cache[i][j]+1;
                 }
+                res=Math.max(res,cache[j][k]);
+
             }
         }
-        return res;
-    }
-    private static int solve(int j, int k, int[] arr, Map<Integer,Integer> map,int cache[][]){
-        if(cache[j][k]!=-1)return cache[j][k];
-        int t=arr[k]-arr[j];
-        if(map.containsKey(t)&&map.get(t)<j){
-            return cache[j][k]= solve(map.get(t),j,arr,map,cache)+1;
-        }
-        return cache[j][k]=2;
+        return res>=3?res:0;
     }
 }
