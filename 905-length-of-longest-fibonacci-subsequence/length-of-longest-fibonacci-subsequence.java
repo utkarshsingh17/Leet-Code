c6815c74b13da3b562d3faef2d1c3521560cf953
Utkarsh Singh <98802787+utkarshsingh17@users.noreply.github.com>
2025-02-27 23:51:13 +0530

    Time: 256 ms (13.61%) | Memory: 55.9 MB (27.81%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
@@ -5,9 +5,13 @@ class Solution {
             map.put(arr[i],i);
         }
         int len=0,res=0;
+        int cache[][]=new int[arr.length][arr.length];
+        for(int[] c:cache){
+            Arrays.fill(c,-1);
+        }
         for(int k=arr.length-1;k>0;k--){
             for(int j=k-1;j>0;j--){
-                len=solve(j,k,arr,map);
+                len=solve(j,k,arr,map,cache);
                 if(len>=3){
                     res=Math.max(res,len);
                 }
@@ -15,11 +19,12 @@ class Solution {
         }
         return res;
     }
-    private static int solve(int j, int k, int[] arr, Map<Integer,Integer> map){
+    private static int solve(int j, int k, int[] arr, Map<Integer,Integer> map,int cache[][]){
+        if(cache[j][k]!=-1)return cache[j][k];
         int t=arr[k]-arr[j];
         if(map.containsKey(t)&&map.get(t)<j){
-            return solve(map.get(t),j,arr,map)+1;
+            return cache[j][k]= solve(map.get(t),j,arr,map,cache)+1;
         }
-        return 2;
+        return cache[j][k]=2;
     }
 }
